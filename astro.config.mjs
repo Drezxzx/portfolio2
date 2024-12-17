5dda09d8caa12e9aa0edbac4e6f55ae5bccabcef
Andres <andres250197@hotmail.com>
2024-12-17 18:40:46 +0100

    add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel';
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()],
+  output : "server",
+  adapter : vercel()
 })
